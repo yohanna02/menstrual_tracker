415d6efd43d079fb0c8953bf1d477e7d7932f50d
Yohanna Abana <yohanaabana02@gmail.com>
2024-03-29 04:12:45 +0100

    first

---
+++ android_app/app/(tabs)/two.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
